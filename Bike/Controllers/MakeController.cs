34335b382bf05fe9a0488d08f85489839def4083
kravindra1415 <kravindra1415@gmail.com>
2022-11-17 12:12:16 +0530

    Add project files.

---
+++ Bike/Controllers/MakeController.cs
@@ -0,0 +1,26 @@
+﻿using Bike.model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bike.Controllers
+{
+    public class MakeController : Controller
+    {
+        //make/bikes
+        public IActionResult Bikes()
+        {
+            Make make = new()
+            {
+                Id = 1,
+                Name = "Harley Davidson"
+            };
+            return View(make);
+
+            //ContentResult contentResult= new ContentResult { Content="Hello World.."};
+            //return contentResult;
+
+            //return Redirect("/home");
+            //return RedirectToAction("About", "Home");
+            //return new EmptyResult();
+        }
+    }
+}
---
+++ Bike/model/Make.cs
@@ -0,0 +1,8 @@
+﻿namespace Bike.model
+{
+    public class Make
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
